b822e12c73fe3a92e42782d0c707a8e066208087
sawangchai saroj <sawangchais@live.com>
2015-05-16 18:26:40 +0700

    add invalid exception test and refractor some test class and method

--- CaptchaTest/CaptchaTest/CaptchaServiceTest.cs
+++ CaptchaTest/CaptchaTest/CaptchaServiceTest.cs
@@ -8,115 +8,251 @@ namespace CaptchaTest
     [TestFixture]
     public class LeftOperandShouldShownAsWordsIfPatternFormRandomReturn1
     {
+        private CaptchaService _captchaService = null;
+        private ICaptchaRandomizer _randomizock = null;
+        private ICaptchaRandomizer _anotherRandomizock = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _captchaService = new CaptchaService();
+            _randomizock = Substitute.For<ICaptchaRandomizer>();
+            _anotherRandomizock = Substitute.For<ICaptchaRandomizer>();
+        }
+
         [Test]
         public void Captcha_ShouldBeONEPlus1_WhenPatternRandomizerReturn1()
         {
-            var captchaService = new CaptchaService();
-            var randomazock = Substitute.For<ICaptchaRandomizer>();
-            randomazock.GetPattern().Returns(1);
-            randomazock.GetOperand().Returns(1);
-            randomazock.GetOperator().Returns(1);
+            _randomizock.GetPattern().Returns(1);
+            _randomizock.GetOperand().Returns(1);
+            _randomizock.GetOperator().Returns(1);
 
-            Assert.AreEqual("ONE+1", captchaService
-                                        .GetCaptcha(randomazock.GetPattern(),
-                                                    randomazock.GetOperand(),
-                                                    randomazock.GetPattern(),
-                                                    randomazock.GetOperand()).ToString());
+            Assert.AreEqual("ONE+1", _captchaService
+                                        .GetCaptcha(_randomizock.GetPattern(),
+                                                    _randomizock.GetOperand(),
+                                                    _randomizock.GetPattern(),
+                                                    _randomizock.GetOperand()).ToString());
         }
 
         [Test]
         public void Captcha_ShouldBeONEPlus2_WhenPatternRandomizerReturn1()
         {
-            var captchaService = new CaptchaService();
-            var randomizock = Substitute.For<ICaptchaRandomizer>();
-            randomizock.GetPattern().Returns(1);
-            randomizock.GetOperand().Returns(1);
-            randomizock.GetOperator().Returns(1);
+            _randomizock.GetPattern().Returns(1);
+            _randomizock.GetOperand().Returns(1);
+            _randomizock.GetOperator().Returns(1);
+            _anotherRandomizock.GetOperand().Returns(2);
 
-            var anotherRandomizock = Substitute.For<ICaptchaRandomizer>();
-            anotherRandomizock.GetOperand().Returns(2);
-
-            Assert.AreEqual("ONE+2", captchaService
-                                        .GetCaptcha(randomizock.GetPattern(),
-                                                    randomizock.GetOperand(),
-                                                    randomizock.GetOperator(),
-                                                    anotherRandomizock.GetOperand()).ToString());
+            Assert.AreEqual("ONE+2", _captchaService
+                                        .GetCaptcha(_randomizock.GetPattern(),
+                                                    _randomizock.GetOperand(),
+                                                    _randomizock.GetOperator(),
+                                                    _anotherRandomizock.GetOperand()).ToString());
         }
 
         [Test]
         public void Captcha_ShouldBeTWOPlus1_WhenPatternRandomizerReturn1()
         {
-            var captchaService = new CaptchaService();
-            var randomizock = Substitute.For<ICaptchaRandomizer>();
-            randomizock.GetPattern().Returns(1);
-            randomizock.GetOperand().Returns(2);
-            randomizock.GetOperator().Returns(1);
+            _randomizock.GetPattern().Returns(1);
+            _randomizock.GetOperand().Returns(2);
+            _randomizock.GetOperator().Returns(1);
+            _anotherRandomizock.GetOperand().Returns(1);
 
-            var anotherRandomizock = Substitute.For<ICaptchaRandomizer>();
-            anotherRandomizock.GetOperand().Returns(1);
-
-            Assert.AreEqual("TWO+1", captchaService
-                                        .GetCaptcha(randomizock.GetPattern(),
-                                                    randomizock.GetOperand(),
-                                                    randomizock.GetOperator(),
-                                                    anotherRandomizock.GetOperand()).ToString());
+            Assert.AreEqual("TWO+1", _captchaService
+                                        .GetCaptcha(_randomizock.GetPattern(),
+                                                    _randomizock.GetOperand(),
+                                                    _randomizock.GetOperator(),
+                                                    _anotherRandomizock.GetOperand()).ToString());
         }
     }
 
+    [TestFixture]
     public class RightOperandShouldShownAsWordsIfPatternFormRandomReturn2
     {
+        private CaptchaService _captchaService = null;
+        private ICaptchaRandomizer _randomizock = null;
+        private ICaptchaRandomizer _anotherRandomizock = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _captchaService = new CaptchaService();
+            _randomizock = Substitute.For<ICaptchaRandomizer>();
+            _anotherRandomizock = Substitute.For<ICaptchaRandomizer>();
+        }
+
         [Test]
         public void Captcha_ShouldBe1PlusOne_WhenPatternRandomizerReturn2()
         {
-            var captchaService = new CaptchaService();
-            var randomazock = Substitute.For<ICaptchaRandomizer>();
-            randomazock.GetPattern().Returns(2);
-            randomazock.GetOperand().Returns(1);
-            randomazock.GetOperator().Returns(1);
+            _randomizock.GetPattern().Returns(2);
+            _randomizock.GetOperand().Returns(1);
+            _randomizock.GetOperator().Returns(1);
 
-            Assert.AreEqual("1+ONE", captchaService
-                                        .GetCaptcha(randomazock.GetPattern(),
-                                                    randomazock.GetOperand(),
-                                                    randomazock.GetOperator(),
-                                                    randomazock.GetOperand()).ToString());
+            Assert.AreEqual("1+ONE", _captchaService
+                                        .GetCaptcha(_randomizock.GetPattern(),
+                                                    _randomizock.GetOperand(),
+                                                    _randomizock.GetOperator(),
+                                                    _randomizock.GetOperand()).ToString());
         }
 
         [Test]
         public void Captcha_ShouldBe2PlusONE_WhenPatternRandomizerReturn2()
         {
-            var captchaService = new CaptchaService();
-            var randomizock = Substitute.For<ICaptchaRandomizer>();
-            randomizock.GetPattern().Returns(2);
-            randomizock.GetOperand().Returns(1);
-            randomizock.GetOperator().Returns(1);
+            _randomizock.GetPattern().Returns(2);
+            _anotherRandomizock.GetOperand().Returns(2);
+            _randomizock.GetOperator().Returns(1);
+            _randomizock.GetOperand().Returns(1);
 
-            var anotherRandomizock = Substitute.For<ICaptchaRandomizer>();
-            anotherRandomizock.GetOperand().Returns(2);
-
-            Assert.AreEqual("2+ONE", captchaService
-                                        .GetCaptcha(randomizock.GetPattern(),
-                                                    anotherRandomizock.GetOperand(),
-                                                    randomizock.GetOperator(),
-                                                    randomizock.GetOperand()).ToString());
+            Assert.AreEqual("2+ONE", _captchaService
+                                        .GetCaptcha(_randomizock.GetPattern(),
+                                                    _anotherRandomizock.GetOperand(),
+                                                    _randomizock.GetOperator(),
+                                                    _randomizock.GetOperand()).ToString());
         }
 
         [Test]
         public void Captcha_ShouldBe1PlusTWO_WhenPatternRandomizerReturn2()
         {
-            var captchaService = new CaptchaService();
-            var randomizock = Substitute.For<ICaptchaRandomizer>();
-            randomizock.GetPattern().Returns(2);
-            randomizock.GetOperand().Returns(1);
-            randomizock.GetOperator().Returns(1);
+            _randomizock.GetPattern().Returns(2);
+            _randomizock.GetOperand().Returns(1);
+            _randomizock.GetOperator().Returns(1);
 
-            var anotherRandomizock = Substitute.For<ICaptchaRandomizer>();
-            anotherRandomizock.GetOperand().Returns(2);
+            _anotherRandomizock.GetOperand().Returns(2);
 
-            Assert.AreEqual("1+TWO", captchaService
-                                        .GetCaptcha(randomizock.GetPattern(),
-                                                    randomizock.GetOperand(),
-                                                    randomizock.GetOperator(),
-                                                    anotherRandomizock.GetOperand()).ToString());
+            Assert.AreEqual("1+TWO", _captchaService
+                                        .GetCaptcha(_randomizock.GetPattern(),
+                                                    _randomizock.GetOperand(),
+                                                    _randomizock.GetOperator(),
+                                                    _anotherRandomizock.GetOperand()).ToString());
+        }
+    }
+
+    [TestFixture]
+    public class InvalidRangExceptionShouldbeThrownIfPatternAndOperandRandomizerReturnAnInvalidRangeValue
+    {
+        private CaptchaService _captchaService = null;
+        private ICaptchaRandomizer _randomizock = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _captchaService = new CaptchaService();
+            _randomizock = Substitute.For<ICaptchaRandomizer>();
+        }
+
+        [Test]
+        public void Captcha_ShouldBeThrownInvalidRangeException_IfRandomPatternIsInvalidMinValue()
+        {
+            _randomizock.GetPattern().Returns(0);
+            _randomizock.GetOperand().Returns(1);
+            _randomizock.GetOperator().Returns(1);
+
+            Assert.Throws(typeof(InvalidRangeException),
+                delegate
+                {
+                    _captchaService.GetCaptcha(_randomizock.GetPattern(),
+                                             _randomizock.GetOperand(),
+                                             _randomizock.GetOperator(),
+                                             _randomizock.GetOperand()).ToString();
+                });
+        }
+
+        [Test]
+        public void Captcha_ShouldBeThrownInvalidRangeException_IfRandomPatternIsInvalidMaxValue()
+        {
+            _randomizock.GetPattern().Returns(3);
+            _randomizock.GetOperand().Returns(1);
+            _randomizock.GetOperator().Returns(1);
+
+            Assert.Throws(typeof(InvalidRangeException),
+                delegate
+                {
+                    _captchaService.GetCaptcha(_randomizock.GetPattern(),
+                                             _randomizock.GetOperand(),
+                                             _randomizock.GetOperator(),
+                                             _randomizock.GetOperand()).ToString();
+                });
+        }
+
+        [Test]
+        public void Captcha_ShouldBeThrownInvalidRangeException_IfRandomOperandIsInvalidMinValue()
+        {
+            _randomizock.GetPattern().Returns(1);
+            _randomizock.GetOperand().Returns(0);
+            _randomizock.GetOperator().Returns(1);
+
+            Assert.Throws(typeof(InvalidRangeException),
+                delegate
+                {
+                    _captchaService.GetCaptcha(_randomizock.GetPattern(),
+                                             _randomizock.GetOperand(),
+                                             _randomizock.GetOperator(),
+                                             _randomizock.GetOperand()).ToString();
+                });
+        }
+
+        [Test]
+        public void Captcha_ShouldBeThrownInvalidRangeException_IfRandomOperandIsInvalidMaxValue()
+        {
+            _randomizock.GetPattern().Returns(1);
+            _randomizock.GetOperand().Returns(99);
+            _randomizock.GetOperator().Returns(1);
+
+            Assert.Throws(typeof(InvalidRangeException),
+                delegate
+                {
+                    _captchaService.GetCaptcha(_randomizock.GetPattern(),
+                                             _randomizock.GetOperand(),
+                                             _randomizock.GetOperator(),
+                                             _randomizock.GetOperand()).ToString();
+                });
+        }
+    }
+
+    [TestFixture]
+    public class InvalidOperatorExceptionShouldbeThrownIfOperatorRandomizerReturnAnInvalidRangeValue
+    {
+        private CaptchaService _captchaService = null;
+        private ICaptchaRandomizer _randomizock = null;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _captchaService = new CaptchaService();
+            _randomizock = Substitute.For<ICaptchaRandomizer>();
+        }
+
+        [Test]
+        public void Captcha_ShouldBeThrownInvalidFormatOperatorException_IfRandomOperatorIsInvalidMinValue()
+        {
+            _randomizock.GetPattern().Returns(1);
+            _randomizock.GetOperand().Returns(1);
+            _randomizock.GetOperator().Returns(0);
+
+            Assert.Throws(typeof(InvalidFormatOperatorException),
+                delegate
+                {
+                    _captchaService.GetCaptcha(_randomizock.GetPattern(),
+                                             _randomizock.GetOperand(),
+                                             _randomizock.GetOperator(),
+                                             _randomizock.GetOperand()).ToString();
+                });
+        }
+
+        [Test]
+        public void Captcha_ShouldBeThrownInvalidFormatOperatorException_IfRandomOperatorIsInvalidMaxValue()
+        {
+            _randomizock.GetPattern().Returns(1);
+            _randomizock.GetOperand().Returns(1);
+            _randomizock.GetOperator().Returns(99);
+
+            Assert.Throws(typeof(InvalidFormatOperatorException),
+                delegate
+                {
+                    _captchaService.GetCaptcha(_randomizock.GetPattern(),
+                                             _randomizock.GetOperand(),
+                                             _randomizock.GetOperator(),
+                                             _randomizock.GetOperand()).ToString();
+                });
         }
     }
 }
